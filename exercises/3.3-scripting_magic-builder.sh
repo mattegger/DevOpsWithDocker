ad801323bdbd89b90b397bf83a2341fff68a4af0
Matt Egger <matt.egger@gmail.com>
2024-11-06 07:01:39 -0500

    exercise 3.3

---
+++ exercises/3.3-scripting_magic-builder.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+GH_REPO=$1
+APP_NAME=`echo $1 | grep -o -P '(?<=/).*(?<=.)'`
+DH_REPO=$2
+
+git clone https://github.com/${GH_REPO}.git
+docker build $APP_NAME -t $APP_NAME
+docker tag ${APP_NAME}:latest ${DH_REPO}/${APP_NAME}:latest
+docker push ${DH_REPO}/${APP_NAME}:latest
