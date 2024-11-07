ee2d475dc5e8adcf83ddb14b87c7fe90a6a1f3d3
Matt Egger <matt.egger@gmail.com>
2024-11-07 06:47:10 -0500

    finished exercise 3.4

--- exercises/3.4-builder.sh
+++ exercises/3.4-builder.sh
@@ -7,4 +7,5 @@ DH_REPO=$2
 git clone https://github.com/${GH_REPO}.git
 docker build $APP_NAME -t $APP_NAME
 docker tag ${APP_NAME}:latest ${DH_REPO}/${APP_NAME}:latest
+docker login -u $DOCKER_USER -p $DOCKER_PWD
 docker push ${DH_REPO}/${APP_NAME}:latest
